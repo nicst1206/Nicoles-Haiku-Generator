ff91c1974ab158f1745b089151f86b37c8079b0e
nicst1206 <nicole@rust-frankfurt.de>
2024-06-27 13:57:02 +0200

    Added basic form Handling and added Typewriter Plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateHaiku(event) {
+  event.preventDefault();
+  new Typewriter("#haiku", {
+    strings: "The haiku will go here",
+    autoStart: true,
+    cursor: "",
+    delay: 10,
+  });
+}
 
+let poemFormElement = document.querySelector("#haiku-generator-form");
+poemFormElement.addEventListener("submit", generateHaiku);
